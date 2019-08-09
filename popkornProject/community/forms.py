a87887a0eb5964902b539354515a2848f993c432
LeeJinho <kartmon@naver.com>
2019-08-09 21:11:15 +0900

    update

--- popkornProject/community/forms.py
+++ popkornProject/community/forms.py
@@ -7,7 +7,6 @@ class CommunityCreate(forms.ModelForm):
         model = Posting
         fields = ['title','content']
         
-
         widgets = {
             'title': forms.TextInput(
                 attrs={'class': 'form-control input_', 'style': 'width: 69%; display:inline; font-size:1em;', 'placeholder': 'TITLE'}
